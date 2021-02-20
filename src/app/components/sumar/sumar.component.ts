ba0bac60d2a21b885f3f450edab167deddfc2e96
harm <harmendoza@gmail.com>
2021-02-20 11:43:00 -0500

    cambiando nombres para lucir mejor

--- src/app/components/sumar/sumar.component.ts
+++ src/app/components/sumar/sumar.component.ts
@@ -7,21 +7,24 @@ import { FormControl, FormGroup, FormBuilder, FormArray } from '@angular/forms';
   styleUrls: ['./sumar.component.css'],
 })
 export class SumarComponent {
+
   formulario: FormGroup;
+
   constructor(private _fb: FormBuilder) {
     this.formulario = this._fb.group({
       cantidades: this._fb.array([]),
     });
     this.getControl();
   }
-  get getCantidad() {
+
+  get cantidades() {
     return this.formulario.get('cantidades') as FormArray;
   }
   add() {
-    const control = this._fb.group({ cantidad: ['1'] });
-    this.getCantidad.push(control);
+    const cantidad = this._fb.group({ cantidad: ['1'] });
+    this.cantidades.push(cantidad);
   }
-  cambio: number;
+  cambio: number[];
   getControl() {
     this.formulario.valueChanges.subscribe((control) => {
       this.cambio = control;
